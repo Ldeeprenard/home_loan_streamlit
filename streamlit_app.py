4db20b6a83ec407aafebb9a0bc07a321b953e5f9
Ldeeprenard <89404729+Ldeeprenard@users.noreply.github.com>
2023-01-05 16:17:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,6 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+df = pd.read_csv ("clients_test.csv")
 
-df
+df.head()
